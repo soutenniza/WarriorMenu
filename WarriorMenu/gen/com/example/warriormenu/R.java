e3eca45d6b330c9fad3950a05f6080305d0a7324
kmark21 <fb3222@wayne.edu>
2014-03-29 19:35:51 -0400

    Changed wayne_green and added wayne_yellow

--- WarriorMenu/gen/com/example/warriormenu/R.java
+++ WarriorMenu/gen/com/example/warriormenu/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int black=0x7f040000;
         public static final int black_overlay=0x7f040001;
         public static final int wayne_green=0x7f040002;
+        public static final int wayne_yellow=0x7f040003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
