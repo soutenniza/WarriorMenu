8f503b924d406928651673221aff00c473b77f5c
soutenniza <vannguyen@Vans-MacBook-Air.local>
2014-03-29 15:59:35 -0400

    Changed Menu Background to Wayne Green

--- WarriorMenu/gen/com/example/warriormenu/R.java
+++ WarriorMenu/gen/com/example/warriormenu/R.java
@@ -13,6 +13,7 @@ public final class R {
     public static final class color {
         public static final int black=0x7f040000;
         public static final int black_overlay=0x7f040001;
+        public static final int wayne_green=0x7f040002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
