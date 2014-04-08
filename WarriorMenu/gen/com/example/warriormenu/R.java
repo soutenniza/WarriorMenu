63f5a14f47916e0802b83cab592ea466a8319e0c
soutenniza <vannguyen@Vans-MacBook-Air.local>
2014-04-08 19:43:15 -0400

    Fixed stuff and changed min version to 14

--- WarriorMenu/gen/com/example/warriormenu/R.java
+++ WarriorMenu/gen/com/example/warriormenu/R.java
@@ -31,7 +31,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f090000;
+        public static final int GridLayout1=0x7f090000;
         public static final int action_settings=0x7f090003;
         public static final int button1=0x7f090002;
         public static final int textView1=0x7f090001;
